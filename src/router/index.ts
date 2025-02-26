1542e8c6b4d554d3c831485af283c6e5bf300bb6
Alexey <skejing@yandex.ru>
2025-02-26 15:55:10 +0400

    Создал проект, убрал лишнее, создал переменные, миксины, подключил шрифты, верстка главной страницы

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import NotesView from "@/views/NotesView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: NotesView
   },
 ]
 
